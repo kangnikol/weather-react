ce995eca5a6495ce23d38a6b5af353e1b4953af9
kangnikol <nicholasalvisaputra@gmail.com>
2023-03-21 14:06:01 +0700

    fixxxxxxxxx

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
-defineConfig({
+export default defineConfig({
   plugins: [react()],
 })
