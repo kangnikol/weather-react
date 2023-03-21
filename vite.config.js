25829f93627442ca6114b6885baa49543b217ced
kangnikol <nicholasalvisaputra@gmail.com>
2023-03-21 14:04:42 +0700

    mfx

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import dotenv from "dotenv"
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  dotenv.config({ path: `.env.${mode}` })
-
-  return defineConfig({
-    plugins: [react()],
-  })
-}
+defineConfig({
+  plugins: [react()],
+})
